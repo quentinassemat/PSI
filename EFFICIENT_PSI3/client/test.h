d96f369f52094f7eb29655088fa1a903a0a74b03
quentinassemat <quentin.assemat@icloud.com>
2022-02-02 16:44:42 +0100

    new protocol

---
+++ EFFICIENT_PSI3/client/test.h
@@ -0,0 +1,16 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include "client.h"
+#include "../../TCP/tcp.h"
+#include "../../profil/profil.h"
+#include "../../../../pbc-0.5.14/include/pbc.h"
+#include "../../../../pbc-0.5.14/include/pbc_test.h"
+
+int test_tcp();
+int test_profil();
+int test_pbc();
+int test_hash();
+int test_client();
+
+#endif // TEST_H
---
+++ EFFICIENT_PSI3/server/server.h
@@ -0,0 +1,29 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include "test.h"
+#include "../../TCP/tcp.h"
+#include "../../profil/profil.h"
+#include "../../profil/buffer.h"
+#include "../../../../pbc-0.5.14/include/pbc.h"
+#include "../../../../pbc-0.5.14/include/pbc_test.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define generator_hash "0123456789"
+#define RANDOM_DATA 5000
+#define NONCE_SIZE 1
+
+typedef struct server
+{
+    pairing_t pairing; // paramètre pour la bonne courbe elliptique
+    element_t g; // générateur g commun au client et au server surla même courbe elliptique definié par pairing
+    profil_t data; // correspond aux si du server
+} server;
+
+int server_init(server * server);
+
+#endif // SERVER_H
